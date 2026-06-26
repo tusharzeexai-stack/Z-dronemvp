f355f14db819fd72aa8e1c2fec25f8b3de457252
TusharGupta2820 <tusharwebdevp@gmail.com>
2026-06-26 13:56:39 +0530

    Configure CORS for S3 bucket and add crossOrigin to video tag

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1284,6 +1284,7 @@ function Dashboard({ onLogout }) {
                         ref={videoRef}
                         key={videoPath}
                         src={videoPath === 'v3_x_1test.mp4' ? 'https://zdrone-storage.s3.ap-south-1.amazonaws.com/v3_x_1test.mp4' : `/${videoPath}`}
+                        crossOrigin="anonymous"
                         className="w-full h-full object-contain cursor-pointer"
                         autoPlay
                         loop
